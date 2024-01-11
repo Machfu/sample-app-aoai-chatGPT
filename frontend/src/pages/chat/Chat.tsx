f9528d7929d428a8e01100f162e7abb30db89c12
Chinmay Shete <chinmay.shete@machfu.com>
2024-01-11 17:54:19 -0500

    Added Machfu specific title, description etc.

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,6 +10,7 @@ import { isEmpty } from "lodash-es";
 
 import styles from "./Chat.module.css";
 import Contoso from "../../assets/Contoso.svg";
+import Machfu from "../../assets/Machfu.svg";
 
 import {
     ChatMessage,
@@ -586,12 +587,12 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Contoso}
+                                    src={Machfu}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Machfu Knowledge Base</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Ask questions on product manuals, user guides, technical memos etc.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
